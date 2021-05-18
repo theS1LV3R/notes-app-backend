16563685f4752b0eebf20f4ba39ef72926c24eeb
S1LV3R <s1lv3r@corax.team>
2021-05-18 21:34:53 +0200

    fix: note entity

--- src/notes/entities/note.entity.ts
+++ src/notes/entities/note.entity.ts
@@ -1,9 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ObjectId } from 'mongoose';
+import { Entity, Column, ObjectIdColumn } from 'typeorm';
 
 @Entity()
 export class Note {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @ObjectIdColumn()
+  id: ObjectId;
 
   @Column()
   title: string;
